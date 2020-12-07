74c7719914340d91b2a136c45c6f3b296fff328a
Maksim Bulat <m.bulat@itransition.com>
2020-12-07 20:13:05 +0300

    Initial commit

---
+++ GitHubTest/GitHubTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Maksim Bulat on 7.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
